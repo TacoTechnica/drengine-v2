c52b108e748286a5b8a8c9d02b32b642ece1415f
Adris Jautakas <adrisj7@gmail.com>
2021-01-05 19:50:48 -0500

    Very minor image view improvements

--- DR Engine v2/Editor/SubWindows/Resources/SpriteResourceWindow.cs
+++ DR Engine v2/Editor/SubWindows/Resources/SpriteResourceWindow.cs
@@ -2,6 +2,7 @@ using System;
 using DREngine.Editor.SubWindows.FieldWidgets;
 using GameEngine;
 using GameEngine.Game;
+using Gdk;
 using Gtk;
 
 namespace DREngine.Editor.SubWindows.Resources
@@ -22,12 +23,19 @@ namespace DREngine.Editor.SubWindows.Resources
 
         protected override void OnInitialize(Box container)
         {
+            ScrolledWindow scroll = new ScrolledWindow();
+            scroll.MinContentWidth = 300;
+            scroll.MinContentHeight = 300;
             _image = new Image();
+            scroll.Add(_image);
             _label = new Text("Nothing loaded");
             _fields = new ExtraDataFieldBox(_editor, typeof(Sprite), true);
-            container.PackStart(_image, true, true, 4);
+            
+            container.PackStart(scroll, true, true, 16);
+            //container.PackStart(_image, true, true, 4);
             container.PackEnd(_label, false, false, 4);
             container.PackEnd(_fields, false, true, 4);
+            scroll.Show();
             _image.Show();
             _label.Show();
             _fields.Show();
@@ -39,6 +47,12 @@ namespace DREngine.Editor.SubWindows.Resources
         {
             // Load sprite
             _image.File = resource.Path;
+
+            // Scale
+            Pixbuf old = _image.Pixbuf;
+            _image.Pixbuf = _editor.Icons.ScaleToRegularSize(_image.Pixbuf, 300);
+            old.Dispose();
+
             _label.Text = $"{resource.Width} x {resource.Height} Sprite";
             _fields.LoadTarget(resource);
         }
